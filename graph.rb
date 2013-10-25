562367820eef0fbfbe31572cd65b819602748736
Isay Sosa <yasnet21@gmail.com>
2013-10-25 16:41:32 -0500

    Graph files

---
+++ graph.rb
@@ -0,0 +1,22 @@
+class Graph
+  attr_accessor :g
+
+  def initialize
+    @g = {}
+  end
+
+  def add_node(node)
+    @g[node.key] = node
+  end
+
+  def add_nodes(nodes = [])
+    nodes.each { |node| add_node node }
+  end
+
+  def add_conection(node_origin, node_destination, cost)
+    @g[node_origin.key] = node_origin if @g[node_origin.key].nil?
+    @g[node_destination.key] = node_destination if @g[node_destination.key].nil?
+
+    @g[node_origin.key] = Edge.new(node_origin, node_destination, cost)
+  end
+end
